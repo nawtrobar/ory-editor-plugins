c2ab65fc043e688a9695fe49ff4a1a72fce4413b
nawtrobar <nawtrobar@posteo.net>
2018-06-28 22:40:36 +0200

    add datatypes in input tags

--- plugins/content/inputChallenge/src/Component/Display/index.js
+++ plugins/content/inputChallenge/src/Component/Display/index.js
@@ -10,19 +10,66 @@ class Display extends Component {
 	}
 
 	render() {
-		return (
-			<div class="text-exercise">
-				<form class="input-challenge-group">	
-					<div class="input-challenge-input-wrapper pull-right">
-						<input class="input-challenge-input" 
-							type="text"
-							placeholder="Deine Lösung"
-						/>
-					</div>
+		if(this.props.state.solutionType==="string"){
+			return (
+				<div class="text-exercise">
+					<form class="input-challenge-group">	
+						<div class="input-challenge-input-wrapper pull-right">
+							<input class="input-challenge-input" 
+								data-type="input-string-equal-match-challenge"
+								type="text"
+								placeholder="Deine Lösung"
+							/>
+						</div>
 				</form>
-			</div>
-		)
+		</div>
+			)
+		}
+
+		else if(this.props.state.solutionType==="expression"){
+			return (
+				<div class="text-exercise">
+					<form class="input-challenge-group">	
+						<div class="input-challenge-input-wrapper pull-right">
+							<input class="input-challenge-input" 
+								data-type="input-expression-equal-match-challenge"
+								type="text"
+								placeholder="Deine Lösung"
+							/>
+						</div>
+				</form>
+		</div>
+			)
+		}
+		else if(this.props.state.solutionType==="number"){
+			return (
+				<div class="text-exercise">
+					<form class="input-challenge-group">	
+						<div class="input-challenge-input-wrapper pull-right">
+							<input class="input-challenge-input" 
+								data-type="input-number-equal-match-challenge"
+								type="text"
+								placeholder="Deine Lösung"
+							/>
+						</div>
+				</form>
+		</div>
+			)
+		}
+		else {
+			return (
+				<div class="text-exercise">
+					<form class="input-challenge-group">	
+						<div class="input-challenge-input-wrapper pull-right">
+							MUST SELECT DATATYPE
+							/>
+						</div>
+				</form>
+		</div>
+			)
+		}
 	}
+
 }
 
 export default Display
